2918d641705e0ee20188ca13330b2dd0198526d6
Lawrence Gumabon <116777078+lawrencegumabon@users.noreply.github.com>
2023-08-27 13:44:11 +0800

    🎉 UPDATED

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function addTask() {
     // FOR DONE BUTTON
     let doneBttn = document.createElement("button");
     doneBttn.className =
-      "bg-[#839AC3] py-1 px-4 rounded-full font-bold text-sm shadow-lg active:scale-105";
+      "bg-[#839AC3] py-1 px-4 rounded-full font-bold text-sm shadow-lg";
     doneBttn.innerHTML = "DONE";
     console.log(doneBttn.value);
     listCont.appendChild(doneBttn);
